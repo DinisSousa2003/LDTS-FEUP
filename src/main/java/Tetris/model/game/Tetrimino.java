a6c737512e84cc53555399d88c2c93620838e950
Miguel_Teixeira <teixemigue@gmail.com>
2022-01-13 19:05:56 +0000

    Tetriminos models done

--- src/main/java/Tetris/model/game/Tetrimino.java
+++ src/main/java/Tetris/model/game/Tetrimino.java
@@ -1,4 +1,56 @@
 package Tetris.model.game;
 
-public class Tetrimino {
+import Tetris.model.Position;
+import com.googlecode.lanterna.TextColor;
+
+public abstract class Tetrimino {
+
+    protected TetriminoDirection direction;
+    private Position centralPosition;
+
+    public Tetrimino(Position position){
+        this.direction = TetriminoDirection.UP;
+        this.centralPosition = new Position(position.getX(), position.getY());
+
+    }
+
+    public abstract Position[] getPositions();
+
+
+
+    public void  rotateRight(){
+        switch(this.direction){
+            case UP:
+                this.direction = TetriminoDirection.RIGHT;
+                break;
+            case RIGHT:
+                this.direction = TetriminoDirection.DOWN;
+                break;
+            case DOWN:
+                this.direction = TetriminoDirection.LEFT;
+                break;
+            case LEFT:
+                this.direction = TetriminoDirection.UP;
+                break;
+
+        }
+
+
+    }
+    public void rotateLeft(){
+        switch(this.direction) {
+            case UP:
+                this.direction = TetriminoDirection.LEFT;
+                break;
+            case LEFT:
+                this.direction = TetriminoDirection.DOWN;
+                break;
+            case DOWN:
+                this.direction = TetriminoDirection.RIGHT;
+                break;
+            case RIGHT:
+                this.direction = TetriminoDirection.UP;
+                break;
+        }
+    }
 }
