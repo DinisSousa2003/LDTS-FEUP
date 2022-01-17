8851a6e968250e8fb36be2477c30007e81c29feb
Miguel_Teixeira <teixemigue@gmail.com>
2022-01-17 17:44:32 +0000

    game viewer almost done

--- src/main/java/Tetris/viewer/GameViewer.java
+++ src/main/java/Tetris/viewer/GameViewer.java
@@ -14,20 +14,46 @@ public class GameViewer extends Viewer<Screen> {
     @Override
     //TODO: DRAW THE MODEL OBJECTS ON THE INTERFACE
     protected void drawElements(GUI gui) throws IOException {
-        for(int i = 0; i < 22; i++) {
-            gui.drawSquare(new Position(i, 0), color.getRAINBOW().get(i % 7));
+        for(int i = 0; i < 12; i++) {
+            gui.drawSquare(new Position(i, 0), color.getColor("LIGHT_GRAY"));
         }
 
-        for(int i = 0; i < 22; i++) {
-            gui.drawSquare(new Position(i, 21), color.getRAINBOW().get(i % 7));
-        }
-
-        for(int i = 1; i < 21; i++) {
-            gui.drawSquare(new Position(0, i), color.getRAINBOW().get(i % 7));
+        for(int i = 0; i < 12; i++) {
+            gui.drawSquare(new Position(i, 21), color.getColor("LIGHT_GRAY"));
         }
 
         for(int i = 1; i < 21; i++) {
-            gui.drawSquare(new Position(21, i), color.getRAINBOW().get(i % 7));
+            gui.drawSquare(new Position(0, i), color.getColor("LIGHT_GRAY"));
         }
+
+        for(int i = 1; i < 21; i++) {
+            gui.drawSquare(new Position(11, i), color.getColor("LIGHT_GRAY"));
+        }
+
+
+        gui.drawText(new Position(13, 1), "P", color.getColor("RED"));
+        gui.drawText(new Position(14, 1), "O", color.getColor("ORANGE"));
+        gui.drawText(new Position(15, 1), "I", color.getColor("YELLOW"));
+        gui.drawText(new Position(16, 1), "N", color.getColor("GREEN"));
+        gui.drawText(new Position(17, 1), "T", color.getColor("BLUE"));
+        gui.drawText(new Position(18, 1), "S", color.getColor("PURPLE"));
+
+        gui.drawText(new Position(14, 3), "0000", color.getColor("WHITE"));
+
+        gui.drawText(new Position(13, 5), "LEVEL", color.getColor("YELLOW"));
+        gui.drawText(new Position(20, 5), "0", color.getColor("WHITE"));
+
+        gui.drawText(new Position(13, 7), "LINES", color.getColor("YELLOW"));
+        gui.drawText(new Position(19, 7), "00", color.getColor("WHITE"));
+
+        for(int i = 14; i<20;i++)
+        {
+            for(int j = 10; j < 20;j++)
+            {
+                gui.drawSquare(new Position(i, j), color.getColor("GRAY"));
+            }
+        }
+        int[] fila = {};
+        for(int i = 11; i < 19;i++)
     }
 }
